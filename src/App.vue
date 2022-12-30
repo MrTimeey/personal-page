50f59af10bd33ccc7edb80ea1b7552a0a4da6c0d
Tim Siegler <Tim.Siegler@fincon.eu>
2022-12-30 02:51:29 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
-<script setup></script>
+<script setup>
+// eslint-disable-next-line no-unused-vars
+import Profile from './components/Profile.vue';
+</script>
 
 <template>
-    <div>
-        <h1 class="text-blue-600/100">TIM</h1>
-        <h1 class="text-3xl font-bold underline">TEST</h1>
-    </div>
+    <h1 class="heading-1">TIM</h1>
+    <!--    <Profile></Profile>-->
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    /*colors: {
+      primary: "var(--primary)",
+      secondary: "var(--secondary)",
+      neutral: "var(--neutral)"
+    },*/
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
